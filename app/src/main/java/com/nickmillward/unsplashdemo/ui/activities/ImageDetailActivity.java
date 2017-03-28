03dac701625ec94e8c8974dd68353094bfd80f01
Nick Millward <nicholasmillward@gmail.com>
2017-03-28 18:23:51 -0400

    set hero image in detailActivity

--- app/src/main/java/com/nickmillward/unsplashdemo/ui/activities/ImageDetailActivity.java
+++ app/src/main/java/com/nickmillward/unsplashdemo/ui/activities/ImageDetailActivity.java
@@ -3,15 +3,22 @@ package com.nickmillward.unsplashdemo.ui.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.nickmillward.unsplashdemo.R;
 import com.nickmillward.unsplashdemo.ui.views.ImageDetailView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ImageDetailActivity extends AppCompatActivity implements ImageDetailView {
 
     public static final String KEY_USER_NAME = "userName";
     public static final String KEY_USER_LOCATION = "userLocation";
     public static final String KEY_PHOTO_URL = "photoUrl";
+
+    @BindView(R.id.iv_detail_hero_image) ImageView iv_detail_hero_image;
 
     private String userName;
     private String userLocation;
@@ -23,8 +30,10 @@ public class ImageDetailActivity extends AppCompatActivity implements ImageDetai
         setContentView(R.layout.activity_image_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         initIntentExtras();
+        initViewsWithExtras();
 
     }
 
@@ -35,6 +44,13 @@ public class ImageDetailActivity extends AppCompatActivity implements ImageDetai
             userLocation = extras.getString(KEY_USER_LOCATION);
             photoUrl_regular = extras.getString(KEY_PHOTO_URL);
         }
+    }
+
+    private void initViewsWithExtras() {
+        Glide.with(this)
+                .load(photoUrl_regular)
+                .centerCrop()
+                .into(iv_detail_hero_image);
     }
 
     @Override
